d88806b6ca490a67d5578bd0a8ce87d940a3222d
jhonathanmusa <jonathansm891@gmail.com>
2020-08-19 22:25:22 -0500

    fourth commit

--- main.js
+++ main.js
@@ -3,40 +3,34 @@ const elementos = formulario.elementos;
 const boton = document.getElementById("btn");
 
 // Precios
-// const precios = { sencilla: 20000, doble: 25000, triple: 28000 };
+const precios = { sencilla: 20000, doble: 25000, triple: 28000 };
 
-// Valida el pedido, analiza si el campo esta vacio o no
-const validarPedido = (e) => {
-  var pedido = formulario.pedido.value;
+// funcion principal que activa los eventos del formulario
+const validar = (e) => {
+  let pedido = formulario.pedido.value;
+  let cantidad = formulario.cantidad.value;
+  let terminos = formulario.terminos.checked;
+
+  // Valida el pedido, analiza si el campo esta vacio o no
   if (pedido == 0) {
     alert("El pedido no puede estar vacio");
     // impide que la pagina se actualice despues de validar
     e.preventDefault();
   }
-};
 
-// valida si se ingresa una cantidad de elementos
-const validarCantidad = (e) => {
-  var cantidad = formulario.cantidad.value;
+  // valida si se ingresa una cantidad de elementos
   if (cantidad == 0) {
     alert("La cantidad no puede estar vacia");
     e.preventDefault();
   }
-};
 
-const validarTerminos = (e) => {
-  if (formulario.terminos.checked == true) {
+  if (terminos == true) {
   } else {
     alert("Aceptar los terminos y condiciones");
     e.preventDefault();
   }
+
+  formulario.resultado.value = "TOTAL: " + cantidad * precios[pedido];
 };
 
-// funcion principal que activa los eventos del formulario
-const validar = (e) => {
-  validarPedido(e);
-  validarCantidad(e);
-  validarTerminos(e);
-};
-
-formulario.addEventListener("submit", validar);
+boton.addEventListener("click", validar);
